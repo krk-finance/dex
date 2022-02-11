e8896e925dbcf8d7f1b37bcc5626bdcaa30f8216
user <esy_win@hotmail.com>
2022-02-11 04:18:33 +0100

    initial krk commit

---
+++ precache-manifest.2bce22be2ec502162b114bd538d78a55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73327fcb27bde508aa6f19b38fb8a80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16a1a0acc87db7fea603",
+    "url": "/static/css/2.45c40c98.chunk.css"
+  },
+  {
+    "revision": "f394cbaf93b0ab76d78b",
+    "url": "/static/css/main.0f309f38.chunk.css"
+  },
+  {
+    "revision": "16a1a0acc87db7fea603",
+    "url": "/static/js/2.601bbcf9.chunk.js"
+  },
+  {
+    "revision": "958608aaef3e81c9b20a22457416f406",
+    "url": "/static/js/2.601bbcf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f394cbaf93b0ab76d78b",
+    "url": "/static/js/main.f3122c93.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "e2e3be35b35e131bc5369041b347d51e",
+    "url": "/static/media/logo.e2e3be35.png"
+  }
+]);
